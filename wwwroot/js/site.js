a92e4223f30e759f1e56f5105d5f1fe47579180e
Capoto <47067142+Capoto@users.noreply.github.com>
2023-03-05 14:25:58 -0300

    Mudanças no CSS e metódo paginate implementado

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -2,3 +2,10 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+$(document).ready(function () {
+    $('#dtBasicExample').DataTable({
+        pagingType: 'full_numbers',
+    });
+    
+  });
